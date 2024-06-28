cf8aafe46e0e7fe5adbf297a00f460d9ef209db1
Umar Dachia <omardachia@gmail.com>
2024-06-28 09:24:16 -0700

    init commit

---
+++ app/Models/ExperienceTask.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ExperienceTask extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "title",
+        "descriptiton",
+        "experience_id",
+    ];
+
+    /**
+     * Get the experience that owns the ExperienceTask
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function experience(): BelongsTo
+    {
+        return $this->belongsTo(Experience::class);
+    }
+}
---
+++ database/migrations/2024_06_28_155913_create_education_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('education', function (Blueprint $table) {
+            $table->id();
+            // $table->string("have_education")->nullable();
+            $table->string("education_degree")->default(true);
+            $table->string("education_location")->nullable();
+            $table->longText("education_achievements")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('education');
+    }
+};
